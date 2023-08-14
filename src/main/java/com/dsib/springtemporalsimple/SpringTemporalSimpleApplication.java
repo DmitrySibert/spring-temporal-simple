6fecab3d14b7625c6601acf9b8e1333244ad3395
dmitrii <dimaaasik.s@gmail.com>
2023-08-14 20:23:45 +0600

    Initial commit

---
+++ src/main/java/com/dsib/springtemporalsimple/SpringTemporalSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.dsib.springtemporalsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTemporalSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTemporalSimpleApplication.class, args);
+	}
+
+}
